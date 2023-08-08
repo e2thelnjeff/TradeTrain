f7c98bf2b83ccb4fc733847e123da858591226ab
Owenn-boop <owennherrmann1@gmail.com>
2023-08-07 20:24:11 -0500

    fixed warning about tr being a child of table

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,14 @@ function App() {
   const [index, setIndex] = useState(0);
   const [symbol_days, setSymbolDays] = useState([]);
   const [selected_symbol_day, setSelectedSymbolDay] = useState("2017-09-29_GOOGL");
+  const API_URL = 'http://localhost:3000/api'
+
   if (symbol_days.length == 0) {
     getSymbol_Days()
   }
 
   async function getSymbol_Days(){
-    const response = await fetch('http://localhost:3000/api/get_symbol_days').then((res)=>res.json());
+    const response = await fetch(`${API_URL}/get_symbol_days`).then((res)=>res.json());
     //symbol_days.file_names.forEach((element)=>console.log(element));
     setSymbolDays(response.file_names)
   }
@@ -36,7 +38,7 @@ function App() {
 
     //const postResponse = await fetch('http://localhost:3000/api',postOptions);
 
-    const results = await fetch(`http://localhost:3000/api?count=${count}&symbol_day=${selected_symbol_day}`).then((res)=>res.json());
+    const results = await fetch(`${API_URL}?count=${count}&symbol_day=${selected_symbol_day}`).then((res)=>res.json());
     
     console.log("drum roll");
     console.log("Close price is: " + results.close);
@@ -50,37 +52,39 @@ function App() {
         <h1>{data.symbol}</h1>
         
         <p>
-          <select class="ui dropdown" title='Symbol Day' id='symbo_day_selection' onChange={()=>setSelectedSymbolDay(symbo_day_selection.value)}>
+          <select className="ui dropdown" title='Symbol Day' id='symbo_day_selection' onChange={()=>setSelectedSymbolDay(symbo_day_selection.value)}>
             {symbol_days.map((symbol_day, i)=>{
               return <option className='item' key={i} >{symbol_day}</option>
             })}
           </select>
         </p>
         <table>
-          <tr>
-            <td>Date: </td>
-            <td>{data.date}</td>
-          </tr>
-          <tr>
-            <td>Open: </td>
-            <td>{data.open}</td>
-          </tr>
-          <tr>
-            <td>High: </td>
-            <td>{data.high}</td>
-          </tr>
-          <tr>
-            <td>Low: </td>
-            <td>{data.low}</td>
-          </tr>
-          <tr>
-            <td>Close (Last Price): </td>
-            <td>{data.close}</td>
-          </tr>
-          <tr>
-            <td>Volume: </td>
-            <td>{data.volume}</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Date: </td>
+              <td>{data.date}</td>
+            </tr>
+            <tr>
+              <td>Open: </td>
+              <td>{data.open}</td>
+            </tr>
+            <tr>
+              <td>High: </td>
+              <td>{data.high}</td>
+            </tr>
+            <tr>
+              <td>Low: </td>
+              <td>{data.low}</td>
+            </tr>
+            <tr>
+              <td>Close (Last Price): </td>
+              <td>{data.close}</td>
+            </tr>
+            <tr>
+              <td>Volume: </td>
+              <td>{data.volume}</td>
+            </tr>
+          </tbody>
         </table>
         <br/>
         <p>
