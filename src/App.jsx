b62f3d69bc9d6c9fb55f9545c4c0d0191302f0fd
Owenn-boop <owennherrmann1@gmail.com>
2023-08-07 21:29:50 -0500

    turned table into a component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import GridEqualWidth from './components/GridEqualWidth.jsx'
 import BuyButton from './components/BuyButton'
 import SellButton from './components/SellButton'
 import Input from './components/Input'
+import PriceTable from './components/PriceTable'
 
 function App() {
   const [count, setCount] = useState(0);
@@ -58,34 +59,7 @@ function App() {
             })}
           </select>
         </p>
-        <table>
-          <tbody>
-            <tr>
-              <td>Date: </td>
-              <td>{data.date}</td>
-            </tr>
-            <tr>
-              <td>Open: </td>
-              <td>{data.open}</td>
-            </tr>
-            <tr>
-              <td>High: </td>
-              <td>{data.high}</td>
-            </tr>
-            <tr>
-              <td>Low: </td>
-              <td>{data.low}</td>
-            </tr>
-            <tr>
-              <td>Close (Last Price): </td>
-              <td>{data.close}</td>
-            </tr>
-            <tr>
-              <td>Volume: </td>
-              <td>{data.volume}</td>
-            </tr>
-          </tbody>
-        </table>
+        <PriceTable data={data}/>
         <br/>
         <p>
           <button onClick={() => getQuote()}>Get Next Price</button>
---
+++ src/components/PriceTable.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+function PriceTable(data){
+    data = data.data
+    
+
+    return(
+      <table>
+        <tbody>
+          <tr>
+            <td>Date: </td>
+            <td>{data.date}</td>
+          </tr>
+          <tr>
+            <td>Open: </td>
+            <td>{data.open}</td>
+          </tr>
+          <tr>
+            <td>High: </td>
+            <td>{data.high}</td>
+          </tr>
+          <tr>
+            <td>Low: </td>
+            <td>{data.low}</td>
+          </tr>
+          <tr>
+            <td>Close (Last Price): </td>
+            <td>{data.close}</td>
+          </tr>
+          <tr>
+            <td>Volume: </td>
+            <td>{data.volume}</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+}
+export default PriceTable
