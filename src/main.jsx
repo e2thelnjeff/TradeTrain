3cc866845e25361f3fefd95bb72dc39764c3b42c
Owenn-boop <owennherrmann1@gmail.com>
2023-08-07 21:11:12 -0500

    moved index.css to styles folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import 'semantic-ui-css/semantic.min.css'
+import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
